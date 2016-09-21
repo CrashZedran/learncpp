65f0bdc6935ee09fe6915675336307a8e25ee228
Andres F Robles <crashzedran@gmail.com>
2016-09-21 10:32:11 -0500

    agregando ejemplo con funciones

---
+++ funciones.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+// funcion ejempl funcion 
+// funcion que imprime en pantalla un mensaje
+// no retorna nada
+void EjemploFuncion()
+{
+    cout << "Un ejemplo de Funcion\n";
+}
+
+// funcion main que inicia el programa
+int main()
+{
+    cout << "En main\n";
+    EjemploFuncion();
+    cout << "De vuelta a main\n";
+    return 0;
+}
